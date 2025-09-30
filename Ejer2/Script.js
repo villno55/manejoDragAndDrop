75370414eb0a78473ed5164b53cafdeeac710157
villno55 <adsotrabajos24@gmail.com>
2025-09-30 12:09:45 -0500

    Done

--- Ejer2/Script.js
+++ Ejer2/Script.js
@@ -1,1 +1,13 @@
+let hoy = new Date();
 
+let halween = new Date(hoy.getFullYear(), 9, 31); 
+
+if (hoy > halween) {
+  halween.setFullYear(halween.getFullYear() + 1);
+}
+
+let dif = halween - hoy;
+let d = Math.ceil(dif / (1000 * 60 * 60 * 24));
+
+document.getElementById("res").textContent =
+  "Ya casi.Menos " + d + " días para Halloween 👻";
