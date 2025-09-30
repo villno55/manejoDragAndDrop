30bed0eb1a7629a951d8c946a12fe4878067eefe
villno55 <adsotrabajos24@gmail.com>
2025-09-30 18:08:30 -0500

    Create Script.js

---
+++ Ejer3/Script.js
@@ -0,0 +1,17 @@
+function allow(ev) {
+  ev.preventDefault(); 
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id); 
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  let d = ev.dataTransfer.getData("text");
+  let t = document.getElementById(d);
+
+  if (ev.target.classList.contains("col")) {
+    ev.target.appendChild(t); 
+  }
+}
